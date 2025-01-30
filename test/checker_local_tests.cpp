73ab0058b4f36cdc6bc53bf09ccb77fb757a2fdc
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2025-01-30 14:21:26 -0500

    Add local checker error test

--- test/checker_local_tests.cpp
+++ test/checker_local_tests.cpp
@@ -19,6 +19,7 @@ TEST_CASE("Local variable declarations", "[checker]") {
     Parser parser;
     GlobalChecker global_checker;
     LocalChecker local_checker;
+    Logger::inst().set_printing_enabled(false);
 
     SECTION("Valid local variable declarations") {
         auto file = make_test_code_file("let a = 1");
@@ -28,6 +29,19 @@ TEST_CASE("Local variable declarations", "[checker]") {
         local_checker.check(ast);
 
         CHECK(Logger::inst().get_errors().empty());
+    }
+
+    SECTION("Let type mismatch") {
+        // Logger::inst().set_printing_enabled(true);
+        auto file = make_test_code_file("let a: i32 = true");
+        auto tokens = lexer.scan(file);
+        auto ast = parser.parse(std::move(tokens));
+        global_checker.check(ast);
+        local_checker.check(ast);
+        auto& errors = Logger::inst().get_errors();
+
+        REQUIRE(errors.size() >= 1);
+        CHECK(errors.at(0) == Err::LetTypeMismatch);
     }
 
     lexer.reset();
