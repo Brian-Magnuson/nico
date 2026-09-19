aa609578abe770c368cfadce4d058c94ed60ea2d
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2026-09-19 11:49:11 -0400

    Change nico_rt to use a C++ implementation

--- nico_rt/include/nico_rt/functions.h
+++ nico_rt/include/nico_rt/functions.h
@@ -1,6 +1,8 @@
 #ifndef NICO_RT_FUNCTIONS_H
 #define NICO_RT_FUNCTIONS_H
 
-extern int nico_rt_printf(const char* format, ...);
+extern "C" {
+int nico_rt_printf(const char* format, ...);
+}
 
 #endif // NICO_RT_FUNCTIONS_H
--- nico_rt/src/functions.cpp
+++ nico_rt/src/functions.cpp
@@ -1,9 +1,9 @@
 #include "nico_rt/functions.h"
 
-#include <stdarg.h>
-#include <stdio.h>
+#include <cstdarg>
+#include <cstdio>
 
-extern int nico_rt_printf(const char* format, ...) {
+extern "C" int nico_rt_printf(const char* format, ...) {
     va_list args;
     va_start(args, format);
     int result = vprintf(format, args);
